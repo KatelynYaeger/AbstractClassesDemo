ccb31130e1433d46a3920ad20303b629e4b9ab1e
Katelyn Yaeger <knyaeger93@gmail.com>
2022-10-17 17:25:40 -0500

    initial commit

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ConsoleUI;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "default make";
+        public string Model { get; set; } = "Defeault model";
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is driving virtually");
+        }
+
+    }
+}
+
+
